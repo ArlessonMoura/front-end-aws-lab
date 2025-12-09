755301a21831c7bbae3c32c42f59c990afc58753
ArlessonMoura <74433643+ArlessonMoura@users.noreply.github.com>
2025-12-09 08:57:18 -0300

    att full name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello React!
-          Arlesson
+          Arlesson Moura
         </p>
         <a
           className="App-link"
